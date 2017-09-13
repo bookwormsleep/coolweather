8a903cdf18e5e9c9ca2bf45516d4233497160e92
coder-pig <13631582576@163.com>
2017-09-13 22:21:00 +0800

    255每日一图之前

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,12 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by jackie on 2017/9/11.
+ */
+public class AQI {
+    public AQICity city ;
+    public class AQICity {
+        public String aqi ;
+        public String pm25 ;
+    }
+}
